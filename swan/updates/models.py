5cd483b81bcf9973cabe56468466fee6ee1ef2a8
Raphael Merx <raphaelm@captricity.com>
2016-04-18 16:28:44 -0700

    Make Chat.batch_id and Chat.api_token nullable

--- swan/updates/models.py
+++ swan/updates/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Chat(models.Model):
     chat_id = models.IntegerField()
-    api_token = models.CharField(max_length=100)
-    batch_id = models.IntegerField()
+    api_token = models.CharField(max_length=100, null=True)
+    batch_id = models.IntegerField(null=True)
--- swan/updates/views.py
+++ swan/updates/views.py
@@ -1,8 +1,15 @@
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
 
+from .models import Chat
+
 
 @api_view(http_method_names=['POST'])
 def telegram_webhook(request):
-    print(request)
+    # {'update_id': 104740320, 'message': {'date': 1461021341, 'chat': {'first_name': 'Raphael', 'id': 173413525, 'type': 'private', 'last_name': 'Merx'}, 'text': 'hihi', 'from': {'first_name': 'Raphael', 'id': 173413525, 'last_name': 'Merx'}, 'message_id': 14}}
+    update = request.data
+    chat_id = update['message']['chat']['id']
+    chat = Chat.objects.get_or_create(chat_id=chat_id)
+    print(chat)
+    print(request.data)
     return HttpResponse('')
