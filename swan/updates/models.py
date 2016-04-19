e226f9e092b6270de4b9c73bbdf6d1c9d31047f0
Raphael Merx <raphaelm@captricity.com>
2016-04-18 17:42:19 -0700

    Handle files sent using Telegram

--- swan/updates/models.py
+++ swan/updates/models.py
@@ -1,7 +1,32 @@
+import json
+import requests
+
 from django.db import models
+from django.conf import settings
 
 
 class Chat(models.Model):
     chat_id = models.IntegerField()
     api_token = models.CharField(max_length=100, null=True)
     batch_id = models.IntegerField(null=True)
+
+    @staticmethod
+    def get_file_contents(update):
+        images = update['message']['photo']
+        file_id = max(images, key=lambda image: image['height'])['file_id']
+        getFile_response = requests.get('https://api.telegram.org/bot{}/getFile?file_id={}'.format(
+            settings.TELEGRAM_TOKEN, file_id))
+        getFile_response = json.loads(getFile_response.content.decode())
+        assert getFile_response['ok']
+        file_path = getFile_response['result']['file_path']
+        # download file contents using the file/bot API
+        response = requests.get('https://api.telegram.org/file/bot{}/{}'.format(settings.TELEGRAM_TOKEN, file_path))
+        return response.content
+
+    def upload_file(self, file_contents, file_name):
+        batch_files_endpoint = 'https://shreddr.captricity.com/api/v1/batch/{}/batch-file/'.format(self.batch_id)
+        session = requests.Session()
+        session.headers.update({'Captricity-API-Token': '{}'.format(self.api_token)})
+        files = {'uploaded_file': file_contents}
+        response = session.post(batch_files_endpoint, data={'file_name': file_name}, files=files)
+        return True
--- swan/updates/views.py
+++ swan/updates/views.py
@@ -6,20 +6,23 @@ from .models import Chat
 
 @api_view(http_method_names=['POST'])
 def telegram_webhook(request):
-    # {'update_id': 104740320, 'message': {'date': 1461021341, 'chat': {'first_name': 'Raphael', 'id': 173413525, 'type': 'private', 'last_name': 'Merx'}, 'text': 'hihi', 'from': {'first_name': 'Raphael', 'id': 173413525, 'last_name': 'Merx'}, 'message_id': 14}}
     update = request.data
     chat_id = update['message']['chat']['id']
     chat, _ = Chat.objects.get_or_create(chat_id=chat_id)
-    message_text = update['message']['text']
-    if message_text.startswith('/token '):
-        token = message_text.strip('/token ')
-        chat.api_token = token
-        chat.save()
-    elif message_text.startswith('/batch '):
-        batch_id = message_text.strip('/batch ')
-        # TODO: error message if batch id is not an integer
-        chat.batch_id = int(batch_id)
-        chat.save()
-    print(chat)
-    print(request.data)
-    return HttpResponse('')
+    if 'text' in update['message']:
+        message_text = update['message']['text']
+        if message_text.startswith('/token '):
+            token = message_text.strip('/token ')
+            chat.api_token = token
+            chat.save()
+        elif message_text.startswith('/batch '):
+            batch_id = message_text.strip('/batch ')
+            # TODO: error message if batch id is not an integer
+            chat.batch_id = int(batch_id)
+            chat.save()
+    elif 'photo' in update['message']:
+        # upload the file to Shreddr
+        file_contents = Chat.get_file_contents(update)
+        chat.upload_file(file_contents, file_name=str(update['message']['date']))
+        # TODO: send a message to the user about file upload
+    return HttpResponse()
